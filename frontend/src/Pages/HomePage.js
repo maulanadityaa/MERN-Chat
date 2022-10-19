3ab0a222fa85a519ac303dd119d27cb2fbbadb5f
secretsupper <maulanadityaaa@gmail.com>
2022-10-19 21:25:46 +0700

    design login and register pages

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,7 +1,52 @@
 import React from "react";
+import {
+  Box,
+  Center,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Register from "../components/Authentication/Register";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Poppins">
+          <Center>Let's Get Talk</Center>
+        </Text>
+      </Box>
+      <Box p={4} bg="white" w="100%" borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded">
+          <TabList>
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Register</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Register />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default Homepage;
