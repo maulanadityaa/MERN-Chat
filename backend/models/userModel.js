b0d4432245ee19573a036c3d315622ec9f01cbc8
secretsupper <maulanadityaaa@gmail.com>
2022-10-26 20:17:52 +0700

    add user authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userModel = mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    picture: {
+    pic: {
       type: String,
-      required: true,
       default: "https://cdn-icons-png.flaticon.com/512/3476/3476013.png",
     },
   },
@@ -15,6 +15,18 @@ const userModel = mongoose.Schema(
     timestamps: true,
   }
 );
+
+userModel.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userModel.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userModel);
 
--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,19 @@ const { chats } = require("./data/data");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 dotenv.config();
 connectDB();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use("/api/user", userRoutes);
 
 app.get("/api/chat", (req, res) => {
   res.send(chats);
